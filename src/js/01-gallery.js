5a46ea4e857b2471f3f9c50c0f08d5fd5a4acb1f
zemfira1 <craftyzema@gmail.com>
2023-05-25 22:12:30 -0400

    01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
-
 console.log(galleryItems);
+
+ import SimpleLightbox from "simplelightbox";
+// // Дополнительный импорт стилей
+ import "simplelightbox/dist/simple-lightbox.min.css";
+
+const list = document.querySelector('ul.gallery');
+
+function makeGallery(items) {
+    let listElements = items.map(item => {
+        let itemEl = document.createElement('li');
+        itemEl.classList.add('gallery__item');
+
+        let itemLink = document.createElement('a');
+        itemLink.classList.add('gallery__link');
+        itemLink.href = item.original;
+        itemLink.rel = "noopener noreferrer";
+        itemEl.append(itemLink);
+
+        let imgEl = document.createElement('img');
+        imgEl.classList.add('gallery__image');
+        imgEl.src = item.preview;
+        imgEl.alt = item.description;
+        imgEl.dataset.source = item.original;
+        itemLink.append(imgEl);
+
+        return itemEl;
+    });
+    console.log(listElements);
+    list.append(...listElements);
+    
+};
+makeGallery(galleryItems);
+//console.log(list);
+
+new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250}); 
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onTimeupdate = function (event) { 
+// {
+//     duration: 61.857
+//     percent: 0.049
+//     seconds: 3.034
+//     }
+    const a = event.currentTarget.value;
+    console.log(a);
+};
+
+player.on('timeupdate', onTimeupdate);
+player.off('timeupdate', onTimeupdate);
